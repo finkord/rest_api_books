fb17031f43a0e9610a0f536d791749345e8f81fe
finkord <devfinkord@gmail.com>
2026-03-07 05:45:26 +0200

    update requirement and add redirect from / to /docs

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from api import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
